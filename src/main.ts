9438b2f01f4737ce517db4ca3334ca6afaa15a05
zaakirio <zaakir.ally@protonmail.com>
2022-10-13 18:53:35 +0100

    Updated main to reflect engine logic and added todos

--- src/main.ts
+++ src/main.ts
@@ -1,49 +1,89 @@
 import * as Phaser from "phaser";
 import { Player } from "./Player";
+import { GridControls } from "./GridControls";
+import { GridPhysics } from "./GridPhysics";
+import { Direction } from "./Direction";
+//TODO: Update MAP for varying screen sizes
+//TODO: Update collision layers for scenery artifacts
+//TODO: Update character model
+//TODO: Create buildings and pathways
+
 const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
   active: false,
   visible: false,
   key: "Game",
 };
 
-const CANVAS_WIDTH = 720;
-const CANVAS_HEIGHT = 528;
+const CANVAS_WIDTH = 2100;
+const CANVAS_HEIGHT = 1900;
 
 export class GameScene extends Phaser.Scene {
+  static readonly TILE_SIZE = 48;
   constructor() {
     super(sceneConfig);
   }
 
+  private gridControls: GridControls;
+  private gridPhysics: GridPhysics;
+
   public create() {
-    // Renders game world
+    // Init game world
     const darkFortressTilemap = this.make.tilemap({ key: "dark-fortress-map" });
     darkFortressTilemap.addTilesetImage("Dark Fortress", "tiles");
+    // Init collision layer(s) for game world
     for (let i = 0; i < darkFortressTilemap.layers.length; i++) {
-      const layer = darkFortressTilemap
-        .createLayer(i, "Dark Fortress", 0, 0)
+      const layer = darkFortressTilemap.createLayer(i, "Dark Fortress", 0, 0);
       layer.setDepth(i);
       layer.scale = 3;
     }
-
+    // Init player
     const playerSprite = this.add.sprite(0, 0, "player");
     playerSprite.setDepth(2);
     playerSprite.scale = 3;
+    // Focus camera on player
     this.cameras.main.startFollow(playerSprite);
     this.cameras.main.roundPixels = true;
+    // Set player location
     const player = new Player(playerSprite, new Phaser.Math.Vector2(6, 6));
-
+    this.gridPhysics = new GridPhysics(player, darkFortressTilemap);
+    this.gridControls = new GridControls(this.input, this.gridPhysics);
+    this.createPlayerAnimation(Direction.UP, 90, 92);
+    this.createPlayerAnimation(Direction.RIGHT, 78, 80);
+    this.createPlayerAnimation(Direction.DOWN, 54, 56);
+    this.createPlayerAnimation(Direction.LEFT, 66, 68);
   }
 
-  public update() {}
-
-  public preload() {   
+  public update(_time: number, delta: number) {
+    this.gridControls.update();
+    this.gridPhysics.update(delta);
+  }
+  public preload() {
     this.load.spritesheet("player", "assets/characters.png", {
       frameWidth: 26,
       frameHeight: 36,
     });
-  }
     this.load.image("tiles", "assets/dark_fortress.png");
-    this.load.tilemapTiledJSON("dark-fortress-map", "assets/dark-fortress.json");
+    this.load.tilemapTiledJSON(
+      "dark-fortress-map",
+      "assets/dark-fortress.json"
+    );
+  }
+
+  private createPlayerAnimation(
+    name: string,
+    startFrame: number,
+    endFrame: number
+  ) {
+    this.anims.create({
+      key: name,
+      frames: this.anims.generateFrameNumbers("player", {
+        start: startFrame,
+        end: endFrame,
+      }),
+      frameRate: 10,
+      repeat: -1,
+      yoyo: true,
+    });
   }
 }
 
